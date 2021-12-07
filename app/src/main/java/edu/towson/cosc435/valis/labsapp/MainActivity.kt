4202947d6ed0219765c2a82d94287c4a7c14e029
JaVonne Harrison <javonneharrison@JaVonnes-MacBook-Pro.local>
2021-12-06 21:05:00 -0500

    lab14 completed

--- app/src/main/java/edu/towson/cosc435/valis/labsapp/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc435/valis/labsapp/MainActivity.kt
@@ -28,9 +28,32 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO - 3. Request the Person data from the ContentProviderDemo app
+        val uri = PersonContract.Person.CONTENT_URI
+        val cursor: Cursor? = this.contentResolver.query(
+            uri,
+            PersonContract.Person.PROJECTION_ALL,
+            null,
+            null,
+            PersonContract.Person.SORT_ORDER_DEFAULT
+        )
         // this requires the READ permission and does not require opening the ContentProviderDemo app
-        // TODO - 4. Loop over the data, create a Person object, insert into the _people list
+
+        if(cursor != null) {
+            if(cursor.count > 0){
+                while(cursor.moveToNext()){
+                    //pull out the name and age
+                    val name: String
+                    val age: Int
+
+                    name = cursor.getString(cursor.getColumnIndex(PersonContract.Person.NAME))
+                    age = cursor.getInt(cursor.getColumnIndex(PersonContract.Person.AGE))
+
+                    val person = Person(name, age)
+                    _people = _people + listOf(person)
+                }
+            }
+        }
+
         setContent {
             LabsAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -44,7 +67,12 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        TODO("7. Launch the PersonPicker activity")
+        val pickPersonRequest = registerForActivityResult(PickPerson() ){ person ->
+            _selectedPerson = person
+        }
+
+        pickPersonRequest.launch(Unit)
+
     }
 }
 
@@ -85,7 +113,9 @@ fun TopBar() {
 
 class PickPerson : ActivityResultContract<Unit, Person?>() {
     override fun createIntent(context: Context, input: Unit): Intent {
-        TODO("5. Create the PersonPicker intent")
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = PersonContract.Person.CONTENT_TYPE
+        return intent
     }
 
 
@@ -93,7 +123,15 @@ class PickPerson : ActivityResultContract<Unit, Person?>() {
         if (resultCode != Activity.RESULT_OK) {
             return null
         }
-        TODO("6. Extract the person data from the result and return a Person object")
+
+        val name = result?.extras?.getString(PersonContract.PERSON_NAME_EXTRA, "default")
+        val age = result?.extras?.getInt(PersonContract.PERSON_AGE_EXTRA, 0)
+
+        if(name != null && age != null){
+            return Person(name, age)
+        }else{
+            return null
+        }
     }
 }
 
